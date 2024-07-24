c7a5c29a87f9f34985c6de435b9d9ede387c942e
mohsen <mohsennozari13822003@gmail.com>
2024-07-24 10:35:30 +0330

    weater

---
+++ script.js
@@ -0,0 +1,10 @@
+function showsidbar() {
+    const sidbar = document.querySelector('.sidbar')
+    sidbar.style.display = 'flex'
+}
+
+function hidesidbar() {
+
+    const sidbar = document.querySelector('.sidbar')
+    sidbar.style.display = 'none'
+}
